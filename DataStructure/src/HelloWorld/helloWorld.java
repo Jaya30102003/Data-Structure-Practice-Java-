86ecade5c85dde5e6b98a9b2885e5d8242587c83
Jaya30102003 <jayadharshiniim@gmail.com>
2025-05-16 11:35:27 +0530

    Initial commit

---
+++ DataStructure/src/HelloWorld/helloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package HelloWorld;
+
+/**
+ * 
+ */
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Jayadharshini here!!!");
+	}
+
+}
