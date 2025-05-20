1d60f187cbbe176dac95266f977eb112bc768f48
Jaya30102003 <jayadharshiniim@gmail.com>
2025-05-20 16:58:53 +0530

    Pattern Updation

---
+++ DataStructure/src/Array/SpiralMatrix.java
@@ -0,0 +1,16 @@
+package Array;
+import java.util.*;
+
+public class SpiralMatrix {
+	static Scanner sc = new Scanner(System.in);
+
+	public SpiralMatrix() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the size of the array");
+	}
+
+}
